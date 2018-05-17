826431d882b9705b2b43559b55e112ce17ee4dba
robynocraig <robynocraig@gmail.com>
2018-05-17 00:44:31 -0500

    added spotify functionality and starting on do-what-it-says

--- liri.js
+++ liri.js
@@ -13,6 +13,9 @@ var Spotify = require('node-spotify-api');
 // calling the request npm package to use with OMDB
 var request = require('request');
 
+// Load the fs package to read and write
+var fs = require("fs");
+
 // setting variable for command user inputs
 var command = process.argv[2]
 
@@ -47,6 +50,10 @@ case "movie-this":
   movieRun();
   break;
 
+case "do-what-it-says":
+  useRandom();
+  break;
+
 default:
   noCommand();
   break;
@@ -79,14 +86,27 @@ function tweetsRun() {
 // If the "spotify-this-song" function is called...
 function songRun() {
 
-  // spotify call that returns song info
-  clientSpotify.search({ type: 'track', query: inputText, limit: 1 }, function(err, data) {
+  if (inputText === "") {
+
+    console.log("Artist: Ace of Base");
+    console.log("Song Name: The Sign");
+    console.log("Song Preview: https://p.scdn.co/mp3-preview/70f126c139847335bc03c756c55b80c99892268e?cid=ff6fa10aa84541fda34802ca0f3e1a3d");
+    console.log("Album: The Sign");
+  }
+
+  else {
+    clientSpotify.search({ type: 'track', query: inputText, limit: 1 }, function(err, data) {
       if (err) {
         return console.log('Error occurred: ' + err);
       }
 
-    console.log(data);
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Song Name: " + data.tracks.items[0].name);
+      console.log("Song Preview: " + data.tracks.items[0].preview_url);
+      console.log("Album: " + data.tracks.items[0].album.name);
+
     });
+  }
 }
 
 // If the "movie-this" function is called...
@@ -99,8 +119,6 @@ function movieRun() {
       // If the request is successful (i.e. if the response status code is 200)
       if (!error && response.statusCode === 200) {
 
-        // Parse the body of the site and recover just the imdbRating
-        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
         console.log("Movie Title: " + JSON.parse(body).Title);
         console.log("Release Year: " + JSON.parse(body).Year);
         console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
@@ -135,6 +153,39 @@ function movieRun() {
       }
     });
   }
+}
+
+function useRandom() {
+
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+
+    //We will then re-display the content as an array for later use.
+    console.log(dataArr);
+
+    // Loop through those numbers and add them together to get a sum.
+    for (var i = 0; i < dataArr.length; i++) {
+      randomSong = [1]
+    }
+
+    clientSpotify.search({ type: 'track', query: randomSong, limit: 1 }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Song Name: " + data.tracks.items[0].name);
+      console.log("Song Preview: " + data.tracks.items[0].preview_url);
+      console.log("Album: " + data.tracks.items[0].album.name);
+
+    });
+  });
+
 }
 
 function noCommand() {
